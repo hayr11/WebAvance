ab3826185ff28fa808006244d1b050127f1322e2
DKTClementSZW <clement.szwechowiez@decathlon.com>
2024-03-19 11:00:04 +0100

    feat(light): add light change

--- src/app/components/light-page/light-page.component.ts
+++ src/app/components/light-page/light-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-light-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './light-page.component.html',
   styleUrl: './light-page.component.css'
 })
 export class LightPageComponent {
+
+  light: boolean = false;
+
+  switchLight() {
+    this.light = !this.light;
+  }
 
 }
