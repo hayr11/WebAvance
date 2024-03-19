1e92b528573d8227d804d3e1d0a59e4258da46eb
DKTClementSZW <clement.szwechowiez@decathlon.com>
2024-03-19 12:12:04 +0100

    feat(route): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LightPageComponent} from "./components/light-page/light-page.component";
+import {RacinePageComponent} from "./components/racine-page/racine-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "racine", component: RacinePageComponent},
+  {path: "light", component: LightPageComponent},
+  {path: "**", redirectTo: "light"}
+];
